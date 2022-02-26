877fa7ad974de6eebce255eec67f763f5f1f657e
Noah Zarro <noahzarro@gmail.com>
2022-02-26 12:13:51 +0000

    wifi management implemented

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { AlertController } from '@ionic/angular';
-
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -12,18 +11,62 @@ import { AlertController } from '@ionic/angular';
 export class SettingsPage implements OnInit {
 
   wifis = []
+  new_ssid = ""
+  new_password = ""
 
   ionViewDidEnter() {
     this.getWifis();
   }
 
-  constructor(private http: HttpClient, private alertController: AlertController) { }
+  constructor(private http: HttpClient, private alertController: AlertController, private toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  displayToast(message: string, color: string) {
+    this.toastController.create({
+      message: message,
+      position: 'top',
+      duration: 7500,
+      color: color
+    }).then((toast) => { toast.present() });
+  }
+
   getWifis() {
     this.http.get(environment.baseUrl + 'wifis').toPromise().then((wifi) => { this.wifis = wifi['wifis'] })
+  }
+
+  deleteWifi(ssid) {
+    this.alertController.create({
+      header: 'Würklich lösche?',
+      buttons: [
+        {
+          text: 'Abbreche',
+          role: 'cancel',
+        }, {
+          text: 'Jo',
+          handler: () => {
+            this.wifis.splice(this.wifis.findIndex(cur_wifi => cur_wifi["ssid"] == ssid))
+            this.http.delete(environment.baseUrl + 'wifi/' + ssid).toPromise()
+            this.displayToast("Wifi isch glöscht worde", "danger")
+          }
+        }
+      ]
+    }).then((alert)=>alert.present())
+  }
+
+  addWifi(){
+    const new_wifi_dict = {"ssid": this.new_ssid, "password": this.new_password}
+    this.http.post(environment.baseUrl + 'wifi', new_wifi_dict).toPromise().then(res =>{
+      console.log(res)
+      if (res["update"] == true) {
+        this.displayToast("Wifi isch updated worde", "success")
+      }
+      else{
+        this.displayToast("Wifi isch hinzuegfüegt worde", "success")
+        this.wifis.push(new_wifi_dict)
+      }
+    })
   }
 
   newDay() {
